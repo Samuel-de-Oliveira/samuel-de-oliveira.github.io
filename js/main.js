d239e0c738a9ff9ff5a7d14a6b7f9fee6b12c5ef
Samuel-de-Oliveira <samwolfg13@gmail.com>
2023-02-10 17:37:00 -0300

    Adicionando aviso

--- js/main.js
+++ js/main.js
@@ -3,3 +3,6 @@ function construction() {
 	window.alert("Este botão não funciona ainda.\n"+
 							 "mas não se preucupe, eu trabalharei nisso em breve")
 }
+
+window.alert('O site está em construção!\n'+
+						'então algumas coisas podem estar fora do lugar ou sem funcionar')
