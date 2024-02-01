6794d29cbda1eb0049a2222334745b7bbb4bc0a5
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-02-01 17:20:47 +0000

    Refazendo tudo

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Codigos em JavaScript
-function construction() {
-	window.alert("Este botão não funciona ainda.\n"+
-							 "mas não se preucupe, eu trabalharei nisso em breve")
-}
-
-window.alert('O site está em construção!\n'+
-						'então algumas coisas podem estar fora do lugar ou sem funcionar')
