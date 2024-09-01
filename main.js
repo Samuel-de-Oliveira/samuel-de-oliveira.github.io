5fdb92376f05ce2784dd5a19a2d7ac87fbeed805
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-08-31 22:44:10 -0300

    Idade automática

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-document.title = "Samuel de Oliveira"
+// Constantes
+const currentDate = new Date()
+const currentYear = currentDate.getFullYear()
+
+// Idade
+var idadeBox    = document.querySelector("#idade")
+const birthDate = "2005"
+
+idadeBox.innerHTML = `<b>Idade:</b> ${currentYear - birthDate} Anos`
