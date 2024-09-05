f52053211771a336ade40facf6fd7fdb98c7d11a
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-09-05 18:36:06 -0300

    Consertando responsividade

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const currentYear = currentDate.getFullYear()
 
 // Idade
 var idadeBox    = document.querySelector("#idade")
-const birthDate = "2005"
+const birthDate = 2005
 
 idadeBox.innerHTML = `<b>Idade:</b> ${currentYear - birthDate} Anos`
+
+if (window.screen.width <= 700) {
+   var photo  = document.querySelector("#minha-imagem")
+   var navbar = document.querySelector("nav")
+   photo.remove()
+   navbar.remove()
+}
