cd50dac3c4cbff2c308675eb744581b88a4981b2
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-09-29 13:27:07 -0300

    Editar informações

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const currentYear = currentDate.getFullYear()
 // Idade
 var idadeBox    = document.querySelector("#idade")
 const birthDate = 2005
-
 idadeBox.innerHTML = `<b>Idade:</b> ${currentYear - birthDate} Anos`
 
+// Remover foto e navbar  para dispositivos móveis
 if (window.screen.width <= 700) {
-   var photo  = document.querySelector("#minha-imagem")
-   var navbar = document.querySelector("nav")
+   let photo  = document.querySelector("#minha-imagem")
+   let navbar = document.querySelector("nav")
    photo.remove()
    navbar.remove()
 }
