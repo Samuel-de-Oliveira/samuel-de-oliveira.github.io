82b02436c26c0093b216ab68f0b1620b34fd631d
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-01-08 17:39:30 -0300

    Add Vue.js

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// Constantes
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-
-// Idade
-var idadeBox    = document.querySelector("#idade");
-const birthDate = 2005;
-idadeBox.innerHTML = `<b>Idade:</b> ${currentYear - birthDate} Anos`;
-
 // Remover foto e navbar  para dispositivos móveis
 if (window.screen.width <= 700) {
    let photo  = document.querySelector("#minha-imagem");
@@ -15,19 +6,30 @@ if (window.screen.width <= 700) {
    navbar.remove();
 }
 
+const components = {
+   data() {
+      return {
+         Age: Vue.ref(0),
+      }
+   },
 
-///////// Testes \\\\\\\\\
-// Evento onscroll
-window.onscroll = () => {
-  let Final = document.body.offsetHeight;
-  if (window.innerHeight + window.scrollY >= Final) {
-    console.log('You touched the ground!');
-  }
+   methods: {
+      CalculateAge() {
+         // Constantes
+         const currentDate = new Date();
+         const currentYear = currentDate.getFullYear();
+         console.log('Hello from Vue.js');
+
+         // Idade
+         var idadeBox    = document.querySelector("#idade");
+         const birthDate = 2005;
+         this.Age = `<b>Idade:</b> ${currentYear - birthDate} Anos`;
+      }
+   },
+
+   beforeMount() {
+      this.CalculateAge();
+   },
 };
-// URL Parameters
-const urlParams = new URLSearchParams(
-  window.location.search
-);
-if (urlParams.get('name')) {
-  console.log(urlParams.get('name'));
-}
+
+Vue.createApp(components).mount("#App");
