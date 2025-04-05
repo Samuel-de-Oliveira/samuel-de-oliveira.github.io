891e05a19257b8b0911749584f6304940d9697af
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-04-05 16:03:27 -0300

    Atualizar informações

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Remover foto e navbar  para dispositivos móveis
+// Remover foto e navbar para dispositivos móveis
 if (window.screen.width <= 700) {
    let photo  = document.querySelector("#minha-imagem");
    let navbar = document.querySelector("nav");
@@ -6,12 +6,15 @@ if (window.screen.width <= 700) {
    navbar.remove();
 }
 
+// Componentes do Vue.js
 const components = {
    data() {
       return {
+         // Vai ser calculado
          Age:        Vue.ref(0),
-         PeriodoMat: Vue.ref(2),
-
+         PeriodoMat: Vue.ref(0),
+         
+         // Não é calculado
          EMail:  Vue.ref("samwolfg12@gmail.com"),
          phoneNumber: Vue.ref("(82) 99912-9253"),
          Github:  Vue.ref("@Samuel-de-oliveira"),
@@ -25,18 +28,44 @@ const components = {
          // Constantes
          const currentDate = new Date();
          const currentYear = currentDate.getFullYear();
-         console.log('Hello from Vue.js');
 
          // Idade
          var idadeBox    = document.querySelector("#idade");
          const birthDate = 2005;
          return (currentYear - birthDate);
-      }
+      },
+
+     CalculatePeriodMath() {
+         // Constantes
+         const currentDate  = new Date();
+         const currentYear  = currentDate.getFullYear();
+         const currentMonth = currentDate.getMonth();
+         const startDate    = 2024;
+
+         // Calculate
+         let Period = (currentYear - startDate) * 2;
+         if (currentMonth >= 6) {
+           Period++
+         } 
+
+         return Period;
+     },
    },
 
    beforeMount() {
-      this.Age = this.CalculateAge();
+      // Version
+      console.log(
+        `Vue's version is ${Vue.version}`
+      );
+
+      this.Age = Vue.ref(
+        this.CalculateAge()
+      );
+
+      this.PeriodoMat = Vue.ref(
+        this.CalculatePeriodMath()
+      );
    },
 };
 
-Vue.createApp(components).mount("#App");
+Vue.createApp(components).mount("#App"); // Roda Vue
